8e8928c74643c141a7d3e92411971db8ad7cce8c
JuJin1324 <jujin1324@daum.net>
2025-12-28 15:29:11 +0900

    refactor(part-4): MemberController에서 Either.fold를 사용하도록 리팩토링

--- part-4-pragmatic-fp/src/main/java/com/techfirst/backend/pragmaticfp/api/MemberController.java
+++ part-4-pragmatic-fp/src/main/java/com/techfirst/backend/pragmaticfp/api/MemberController.java
@@ -1,10 +1,9 @@
 package com.techfirst.backend.pragmaticfp.api;
 
-import com.techfirst.backend.pragmaticfp.domain.ErrorCode;
-import com.techfirst.backend.pragmaticfp.service.MemberDto;
 import com.techfirst.backend.pragmaticfp.service.MemberService;
-import io.vavr.control.Either;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,16 +17,13 @@ public class MemberController {
     private final MemberService memberService;
 
     @PutMapping("/{id}/name")
-    public ResponseEntity<?> changeName(@PathVariable Long id, @RequestBody Map<String, String> body) {
-        String newName = body.get("name");
-        
-        Either<ErrorCode, MemberDto> result = memberService.changeName(id, newName);
+    public ResponseEntity<?> changeName(
+            @PathVariable Long id, @RequestBody Map<String, String> body) {
 
-        if (result.isLeft()) {
-            return ResponseEntity.badRequest().body(Map.of("error", result.getLeft().message()));
-        } else {
-            // 변경된 회원 정보를 그대로 반환합니다.
-            return ResponseEntity.ok(result.get());
-        }
+        return memberService
+                .changeName(id, body.get("name"))
+                .fold(
+                        error -> ResponseEntity.badRequest().body(Map.of("error", error.message())),
+                        ResponseEntity::ok);
     }
 }
